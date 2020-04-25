b8a34f3e65cf0676fda54e3869aa1d3cc5534db3
kutec <ku@kutec.co.in>
2020-04-25 09:23:16 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import {
+  BrowserModule
+} from "@angular/platform-browser";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
 
-import { MaterialModule } from './material.module';
-import { UsersComponent } from './users/users.component';
+import { MaterialModule } from "./material.module";
+import { UsersComponent } from "./users/users.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/users', pathMatch: 'full' },
-  { path: 'users', component: UsersComponent }
+  { path: "", redirectTo: "/users", pathMatch: "full" },
+  { path: "users", component: UsersComponent }
 ];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(routes), MaterialModule ],
-  declarations: [ AppComponent, HelloComponent, UsersComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    MaterialModule
+  ],
+  declarations: [AppComponent, HelloComponent, UsersComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
